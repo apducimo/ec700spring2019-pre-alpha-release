e7beca2c2dfc86436bbd9fca4597703aca70191e
aducimo <aducimo@analog.com>
2019-03-31 22:59:16 -0400

    Integrated stream buffer but commented it out to revert back to baseline design.

--- bp_be/src/v/bp_be_mmu/bp_be_mmu_top.v
+++ bp_be/src/v/bp_be_mmu/bp_be_mmu_top.v
@@ -293,6 +293,148 @@ bp_be_dcache
     ,.lce_tr_resp_ready_i(lce_tr_resp_ready_i)
     );
 
+/* -----\/----- EXCLUDED -----\/-----
+    logic [lce_req_width_lp-1:0] lce_sb_req;
+    logic                        lce_sb_req_v;
+    logic                        lce_sb_req_ready;
+
+    logic [lce_resp_width_lp-1:0] lce_sb_resp;
+    logic                         lce_sb_resp_v;
+    logic                         lce_sb_resp_ready;                                 
+
+    logic [lce_data_resp_width_lp-1:0] lce_sb_data_resp;
+    logic                              lce_sb_data_resp_v;
+    logic                              lce_sb_data_resp_ready;
+
+    logic [cce_cmd_width_lp-1:0] sb_lce_cmd;
+    logic                        sb_lce_cmd_v;
+    logic                        sb_lce_cmd_ready;
+
+    logic [cce_data_cmd_width_lp-1:0] sb_lce_data_cmd;
+    logic                             sb_lce_data_cmd_v;
+    logic                             sb_lce_data_cmd_ready;
+
+bp_be_dcache 
+  #(.data_width_p(reg_data_width_lp) 
+    ,.sets_p(lce_sets_p)
+    ,.ways_p(lce_assoc_p)
+    ,.paddr_width_p(paddr_width_p)
+    ,.num_cce_p(num_cce_p)
+    ,.num_lce_p(num_lce_p)
+    )
+  dcache
+   (.clk_i(clk_i)
+    ,.reset_i(reset_i)
+
+    ,.lce_id_i(dcache_id_i)
+
+    ,.dcache_pkt_i(dcache_pkt)
+    ,.v_i(mmu_cmd_v_i)
+    ,.ready_o(dcache_ready)
+
+    ,.v_o(dcache_v)
+    ,.data_o(mmu_resp.data)
+
+    ,.tlb_miss_i(1'b0)
+    ,.ptag_i(ptag_r)
+
+    ,.cache_miss_o(dcache_miss_v)
+    ,.poison_i(chk_psn_ex_i)
+
+    // LCE-CCE interface
+    ,.lce_req_o(lce_sb_req)
+    ,.lce_req_v_o(lce_sb_req_v)
+    ,.lce_req_ready_i(lce_sb_req_ready)
+
+    ,.lce_resp_o(lce_sb_resp)
+    ,.lce_resp_v_o(lce_sb_resp_v)
+    ,.lce_resp_ready_i(lce_sb_resp_ready)
+
+    ,.lce_data_resp_o(lce_sb_data_resp)
+    ,.lce_data_resp_v_o(lce_sb_data_resp_v)
+    ,.lce_data_resp_ready_i(lce_sb_data_resp_ready)
+
+    // CCE-LCE interface
+    ,.lce_cmd_i(sb_lce_cmd)
+    ,.lce_cmd_v_i(sb_lce_cmd_v)
+    ,.lce_cmd_ready_o(sb_lce_cmd_ready)
+
+    ,.lce_data_cmd_i(sb_lce_data_cmd)
+    ,.lce_data_cmd_v_i(sb_lce_data_cmd_v)
+    ,.lce_data_cmd_ready_o(sb_lce_data_cmd_ready)
+
+    // LCE-LCE interface
+    ,.lce_tr_resp_i(lce_tr_resp_i)
+    ,.lce_tr_resp_v_i(lce_tr_resp_v_i)
+    ,.lce_tr_resp_ready_o(lce_tr_resp_ready_o)
+
+    ,.lce_tr_resp_o(lce_tr_resp_o)
+    ,.lce_tr_resp_v_o(lce_tr_resp_v_o)
+    ,.lce_tr_resp_ready_i(lce_tr_resp_ready_i)
+    );
+
+  // ---------------------------------------------------------------------------
+  // Stream Buffer
+  //
+  bp_be_stream_buffer #(
+    .num_cce_p        (num_cce_p),
+    .num_lce_p        (num_lce_p),
+    .lce_addr_width_p (paddr_width_p),
+    .lce_data_width_p (cce_block_size_in_bits_lp),
+    .ways_p           (lce_assoc_p),
+    .sb_depth_p       (4)
+  ) u_bp_be_stream_buffer (
+    // Clocks and Resets
+    .reset_i (reset_i), // (I) Reset, active high
+
+    .clk_i   (clk_i),   // (I) Clock
+
+    // LCE-SB Interface
+    .lce_sb_req_i       (lce_sb_req),       // (I)
+    .lce_sb_req_v_i     (lce_sb_req_v),     // (I)
+    .lce_sb_req_ready_o (lce_sb_req_ready), // (O)
+
+    .lce_sb_resp_i       (lce_sb_resp),       // (I)
+    .lce_sb_resp_v_i     (lce_sb_resp_v),     // (I)
+    .lce_sb_resp_ready_o (lce_sb_resp_ready), // (O)
+
+    .lce_sb_data_resp_i       (lce_sb_data_resp),       // (I)
+    .lce_sb_data_resp_v_i     (lce_sb_data_resp_v),     // (I)
+    .lce_sb_data_resp_ready_o (lce_sb_data_resp_ready), // (O)
+
+    // SB-LCE Interface
+    .sb_lce_cmd_o       (sb_lce_cmd),            // (O)
+    .sb_lce_cmd_v_o     (sb_lce_cmd_v),          // (O)
+    .sb_lce_cmd_ready_i (sb_lce_cmd_ready),      // (I)
+
+    .sb_lce_data_cmd_o       (sb_lce_data_cmd),       // (O)
+    .sb_lce_data_cmd_v_o     (sb_lce_data_cmd_v),     // (O)
+    .sb_lce_data_cmd_ready_i (sb_lce_data_cmd_ready), // (I)
+
+    // SB-CCE Interface
+    .sb_cce_req_o       (lce_req_o),             // (O)
+    .sb_cce_req_v_o     (lce_req_v_o),           // (O)
+    .sb_cce_req_ready_i (lce_req_ready_i),       // (I)
+
+    .sb_cce_resp_o       (lce_resp_o),            // (O)
+    .sb_cce_resp_v_o     (lce_resp_v_o),          // (O)
+    .sb_cce_resp_ready_i (lce_resp_ready_i),      // (I)
+
+    .sb_cce_data_resp_o       (lce_data_resp_o),       // (O)
+    .sb_cce_data_resp_v_o     (lce_data_resp_v_o),     // (O)
+    .sb_cce_data_resp_ready_i (lce_data_resp_ready_i), // (I)
+
+    // CCE-SB Interface
+    .cce_sb_cmd_i       (lce_cmd_i),       // (I)
+    .cce_sb_cmd_v_i     (lce_cmd_v_i),     // (I)
+    .cce_sb_cmd_ready_o (lce_cmd_ready_o), // (O)
+
+    .cce_sb_data_cmd_i       (lce_data_cmd_i),       // (I)
+    .cce_sb_data_cmd_v_i     (lce_data_cmd_v_i),     // (I)
+    .cce_sb_data_cmd_ready_o (lce_data_cmd_ready_o)  // (O)
+  );
+ -----/\----- EXCLUDED -----/\----- */
+
 always_comb 
   begin
     dcache_pkt.opcode      = bp_be_dcache_opcode_e'(mmu_cmd.mem_op);
@@ -307,4 +449,3 @@ assign mmu_resp_v_o    = dcache_v;
 assign mmu_cmd_ready_o = dcache_ready & ~dcache_miss_v;
 
 endmodule : bp_be_mmu_top
-
